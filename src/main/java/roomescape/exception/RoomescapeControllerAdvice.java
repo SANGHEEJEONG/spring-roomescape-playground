bd125675140b0bd121c5cdaddf507c6115d6dcc3
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-11-29 03:02:06 +0900

    [FIX] 9단계 예외 처리 핸들러 추가

--- src/main/java/roomescape/exception/RoomescapeControllerAdvice.java
+++ src/main/java/roomescape/exception/RoomescapeControllerAdvice.java
@@ -2,6 +2,7 @@ package roomescape.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,11 @@ public class RoomescapeControllerAdvice {
         });
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleInvalidTypeException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 
